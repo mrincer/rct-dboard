a175ef42e31cd774b4689f2aeedd750c2c1270c5
Kirk Incer <khincer96@gmail.com>
2023-05-09 12:17:47 -0600

    Top Navigation bar completed

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Navbar from "./components/Navbar";
+import Navbar from "../../components/Navbar";
 
 const Layout = () => {
     return <Box width="100%" height="100%">
